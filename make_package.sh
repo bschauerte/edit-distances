a95574605076c94c4a8767562f088ea5b8b55d16
Boris Schauerte <schauerte@kit.edu>
2014-08-04 12:06:10 +0200

    initial import

---
+++ make_package.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm edit_distances.zip
+zip -r edit_distances.zip edit_distance_damerau_keylist.m edit_distance_levenshtein.m edit_distance_damerau.m         edit_distance_weighted_keylist.m edit_distance_levenshtein_keylist.m edit_distance_weighted.m
